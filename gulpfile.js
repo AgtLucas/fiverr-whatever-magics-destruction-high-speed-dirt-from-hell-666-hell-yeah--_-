aa154b61497049fb15d1a1ab70674408e57cace3
Lucas <AgtLucas@users.noreply.github.com>
2015-06-16 08:46:56 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
+var path = require('path');
 var gulp = require('gulp');
 var template = require('gulp-template');
 var fileinclude = require('gulp-file-include');
+var rename = require("gulp-rename");
 var argv = require('minimist')(process.argv.slice(1));
 
-var emailId = argv.id || '450';
+var emailId = argv.id || '445';
 var emailQuote = argv.quote || 'Boas compras!';
+var emailObj = {quote: emailQuote, emailId: emailId};
 
 gulp.task('copy', function() {
-  gulp.src('./template/**.*')
-    .pipe(gulp.dest('./' + emailId));
+   gulp.src('../template/**.*')
+    .pipe(gulp.dest('../' + emailId));
 });
 
 gulp.task('build', function() {
-  gulp.src('./template/index.html')
+  return gulp.src('../template/template.html')
+    .pipe(template(emailObj))
     .pipe(fileinclude({
       prefix: '@@',
-      basepath: __dirname + '/template'
+      basepath: path.join(__dirname, '../', emailId)
     }))
-    .pipe(template({quote: emailQuote, emailId: emailId}))
-    .pipe(gulp.dest('./' + emailId + '/build'));
+    .pipe(template(emailObj))
+    .pipe(rename(function (path) {
+      path.basename += "-build";
+    }))
+    .pipe(gulp.dest('../' + emailId));
 });
 
 gulp.task('default', ['copy', 'build']);
